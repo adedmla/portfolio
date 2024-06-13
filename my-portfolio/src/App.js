2304fff381b303db568038371fec18bb0a02616f
adedmla <a.adejumobi67@gmail.com>
2024-06-13 13:50:48 -0500

    Just added my name and short description trying to get the bare bones of the site down before adjusting the CSS

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+      <h1>Hey! My name is Adedamola Adejumobi.</h1>
+      <p>
+        I'm an aspiring software engineer and CS Student at Northwestern
+        University. This site is my creative playground and constantly updating.
+        Welcome!
+      </p>
+    </div>
+  );
+}
+
+export default App;
